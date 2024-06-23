c6744715ef61c4a4f532f66e9fd028e1af327f70
tadayuya <yuya.tada@banana-877.com>
2024-06-23 19:57:12 +0900

    スマホの時はヘッダー非表示に変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,15 +19,15 @@ export default class Header extends Component {
 
     return (
       <>
-        <nav className="border-gray-200 dark:bg-gray-900 fixed z-50 w-full">
+        <nav className="border-gray-200 fixed z-50 w-screen hidden md:block">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Splite</span>
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">Splite</span>
             </a>
             <button
               data-collapse-toggle="navbar-default"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-default"
               aria-expanded="false"
               onClick={showMenu}
@@ -45,12 +45,12 @@ export default class Header extends Component {
             </button>
             <div className="hidden w-full md:block md:w-auto max-sm:bg-white" id="navbar-default">
               <ul
-                className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                 <li>
                   <a
                     href="https://blog.splite.biz"
                     target="_blank"
-                    className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-[#316FA5] md:p-0 dark:text-white md:dark:hover:text-[#316FA5] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-[#316FA5] md:p-0"
                   >
                     Blog
                   </a>
@@ -61,7 +61,7 @@ export default class Header extends Component {
                       <>
                         <li>
                           <Link
-                            className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-[#316FA5] md:p-0 dark:text-white md:dark:hover:text-[#316FA5] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            className="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-[#316FA5] md:p-0"
                             activeClass="active"
                             key={menu.name}
                             to={menu.link}
